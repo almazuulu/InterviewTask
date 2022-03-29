60e5cbd5f495d4343a544c210103aaaaac14df07
almazuulu <almazuulu@gmail.com>
2022-03-29 19:00:04 +0600

    Deleted unneccary things

--- Task1.py
+++ Task1.py
@@ -1,51 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-# def parseFromWebsite(link):
-#     response = requests.get(link)
-#     soup = bs(response.text, 'html.parser')
-#     #print(soup)
-#
-#     items = soup.find_all('div', class_ = 'comment')
-#     #print(items)
-#
-#     items2 = soup.find('span', class_= 'commtext c00')
-#     # for i in items2:
-#     #     comments = i.find('span', class_='commtext c00').text.strip()
-#     #     print(comments)
-#
-#     firstParagraph = items2.find('p')
-#     parentText = firstParagraph.find_parent()
-#
-#     print(firstParagraph)
-#     textOfPar = firstParagraph.get_text()
-#
-#     listOfText = textOfPar.split('.')
-#     print(listOfText)
-#
-#     newList = []
-#
-#     for text in listOfText:
-#         if text.startswith('The header'):
-#             break
-#         newList.append(text)
-#
-#     print(newList)
-#     textForTask =  '.'.join(newList)
-#     print(textForTask)
-#
-#     print(textOfPar)
-#     firstParagraph = str(firstParagraph)
-#
-#     listOfPar = firstParagraph.split('<p>')
-#     print(listOfPar)
-#
-#     print(parentText)
-#
-#
-#     # parentFirstPar = firstParagraph.find('p')
-#     # print(parentFirstPar)
-
 def parseFromWebsite(link):
     response = requests.get(link)
     soup = bs(response.text, 'html.parser')
